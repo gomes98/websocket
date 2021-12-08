3e2723bfb94bb96d02a991686428b0073f482524
Mauricio <gomes98@gmail.com>
2021-12-08 09:37:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+
+require('dotenv').config()
+
+const appWs = require('./app-ws');
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`App Express is running!`);
+})
+
+const wss = appWs(server);
+
+setInterval(() => {
+    wss.broadcast({ n: Math.random() });
+}, 1000)
