f584cf29d54990faea5784628be5138e07d737ee
Mauricio <gomes98@gmail.com>
2021-12-12 23:06:45 -0300

    ajustado wbsocket

--- public/app.js
+++ public/app.js
@@ -2,8 +2,16 @@ let history = document.getElementById('history')
 let enviar = document.getElementById('enviar')
 let send = document.getElementById('send')
 
-// let webSocket = new WebSocket("ws://testemauricio.herokuapp.com/wbs?token=123456");
-let webSocket = new WebSocket("ws://localhost:3000/device?device=123456");
+let baseUrl = window.location.href;
+let urlWs = ""
+if (baseUrl.startsWith("https")) {
+    urlWs = `wss${baseUrl.replace("https", "")}`
+} else if (baseUrl.startsWith("http")) {
+    urlWs = `ws${baseUrl.replace("http", "")}`
+}
+
+let webSocket = new WebSocket(`${urlWs}device?device=123456`);
+
 
 webSocket.onopen = function(event) {
     console.log("conectou");
